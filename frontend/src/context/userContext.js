794dfe8aa940f350aacaa627aa72465d6c55b83e
yoshinoashton <yoshinoashton@csu.fullerton.edu>
2024-03-14 13:22:52 -0700

    bug fixes

--- frontend/src/context/userContext.js
+++ frontend/src/context/userContext.js
@@ -3,7 +3,7 @@ import { createContext, useState, useEffect } from "react";
 export const UserContext = createContext({});
 
 export function UserContextProvider({children}) {
-  const [user, setUser] = useState({});
+  const [user, setUser] = useState(null);
   const [token, setToken] = useState('');
 
   useEffect(() => {
