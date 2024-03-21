4823250b599989fb2566799aeaddc56fdfec604c
Anthony Grippi <anthonyjgrippi@gmail.com>
2024-03-21 15:30:29 -0700

    added more inputs

--- frontend/src/pages/ListingCreate.js
+++ frontend/src/pages/ListingCreate.js
@@ -28,40 +28,19 @@ export default function ListingCreate() {
   return (
     <Layout>
       <div className="create-listing-page">
-<<<<<<< HEAD
-        <select
-        options={options}
-        value={selectedOption}
-        onChange={handleChange}></select>
-=======
-        <form className="create-listing-form">
-          <label>
-            Pal Name
-            <input type="text" id="name"/>
-          </label><br></br>
-          <div className="create-listing-form-rarities">
-          Rarity<br></br>
-            <input type="radio" id="rarity-common" value="Common"/>
-            <label for="rarity-common">Common</label><br></br>
-            <input type="radio" id="rarity-uncommon" value="Uncommon"/>
-            <label for="rarity-uncommon">Uncommon</label><br></br>
-            <input type="radio" id="rarity-rare" value="Rare"/>
-            <label for="rarity-rare">Rare</label><br></br>
-            <input type="radio" id="rarity-epic" value="Epic"/>
-            <label for="rarity-epic">Epic</label><br></br>
-            <input type="radio" id="rarity-legendary" value="Legendary"/>
-            <label for="rarity-legendary">Legendary</label><br></br>
-          </div>
-          <label>
-            Image Link
-            <input type="url"></input>
-          </label><br></br>
-          <label>
-            <button type="submit" value="Create Listing">Create Listing</button>
-            <button type="reset" value="Reset Form">Reset Form</button>
-          </label>
-        </form>
->>>>>>> 37b0c93 (adjusted listing page)
+        <Select
+          options={options}
+          value={selectedOption}
+          onChange={handleChange}>
+        </Select>
+        <br></br>
+        <label for="price">Enter the price: $</label>
+        <input name="price" type="number"/>
+        <br></br>
+        <label for="desc">Write a description::</label><br></br>
+        <textarea name="desc" rows="4" cols="50"/>
+        <br></br>
+        <input type="submit" value="Create"/>
       </div>
     </Layout>
   );
