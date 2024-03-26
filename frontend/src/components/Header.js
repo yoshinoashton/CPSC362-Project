62d6ab1df14861ecf36921f339704eb6d061d796
yoshinoashton <yoshinoashton@csu.fullerton.edu>
2024-03-26 16:17:57 -0700

    added wallet feature - not function / updated css for header

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -27,12 +27,14 @@ export default function Header() {
         </div>  
         
         <div className='header__right'>
+          { context.login && (
+            <p>$0.00</p>
+          )}
           <ul className='header__lists'>
               <li className="header__list">
-                { context.username && (
+                { context.login ? (
                   <button className="header__button" onClick={Logout} refresh="true">Logout</button>
-                )}
-                { !context.username && (
+                ) : (
                   <Link className="header__button" to="/login">Login</Link>
                 )}
               </li>
