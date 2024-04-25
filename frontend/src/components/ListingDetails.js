25a394b2e2972ae0493bd1254393a231a32f9f4e
yoshinoashton <yoshinoashton@csu.fullerton.edu>
2024-04-25 15:33:09 -0700

    added type image code - need to add images

--- frontend/src/components/ListingDetails.js
+++ frontend/src/components/ListingDetails.js
@@ -50,16 +50,18 @@ export default function ListingInfo({ listing }) {
       <img src={pal.imageURL} alt={pal.name} />
 
       <div className='listing-details'>
-        <h4>{pal.name}</h4>
-        <Link to={`/user/${userPal.username}`} className='back-button'>{userPal.username}</Link>
-        <p>{pal.type}</p>
+        <div className='listing-title'>
+          <h4>{pal.name}</h4>
+          <img src={`/elements/${pal.type}.jpeg`} alt={pal.type} className='image-type'/>
+        </div>
+        <Link to={`/user/${userPal.username}`} className='link-user'>{userPal.username}</Link>
         <p>{listing.cost}</p>
         <p>{listing.description}</p>
         {traits && traits.map(trait => (
               <Trait trait={trait}/>
           ))}
+        <button onClick={Buy} className='buy-button'>Buy</button>
       </div>
-      <button onClick={Buy}>Buy</button>
 
     </div>
   );
